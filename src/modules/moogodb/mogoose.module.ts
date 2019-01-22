fd5119630c045358d50bcf6a8bd779f4303ea5b4
zewei <543347844@qq.com>
2019-01-22 16:00:57 +0800

    数据库地址

--- src/modules/moogodb/mogoose.module.ts
+++ src/modules/moogodb/mogoose.module.ts
@@ -1,6 +1,6 @@
 
 import { MongooseModule } from "@nestjs/mongoose";
 
-const MongooseForRootModule = MongooseModule.forRoot("mongodb://59.110.152.162:8098/db");
+const MongooseForRootModule = MongooseModule.forRoot("mongodb://127.0.0.1:8098/db");
 
 export { MongooseForRootModule }
